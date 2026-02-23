e9b3e975e0e5aa78c897752687a7b456a5dfbbe1
Tusharkkr <narutoboi41@gmail.com>
2026-02-23 12:20:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function insert_Row() {
             let tr = document.createElement('tr')
             let td1 = document.createElement('td')
             let td2 = document.createElement('td')
-            td1.innerText='New Cell1'
-            td2.innerText='New Cell2'
+            td1.innerText='New Cell 1'
+            td2.innerText='New Cell 2'
             tr.append(td1,td2)
             table.prepend(tr)
         })
